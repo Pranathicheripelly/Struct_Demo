14b8b886e22d25ef427e513951102c9676956d7e
Pranathicheripelly <Cheripellypranathi123@gmail.com>
2023-10-31 13:36:08 +0530

    Add project files.

---
+++ Struct_Demo/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Demo
+{
+   
+
+            
+
+struct Student
+        {
+            public string Name;
+            public int Age;
+            public string Course;
+            public double GPA;
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                doApp();
+            }
+
+            static void doApp()
+            {
+                Student student1;
+                student1.Name = "Nani";
+                student1.Age = 24;
+                student1.Course = "Computer Science";
+                student1.GPA = 5.8;
+
+                Student student2;
+                student2.Name = "babu";
+                student2.Age = 26;
+                student2.Course = "Python";
+                student2.GPA = 8.5;
+
+                Console.WriteLine("Student 1 Details:");
+                Console.WriteLine($"Name: {student1.Name}");
+                Console.WriteLine($"Age: {student1.Age}");
+                Console.WriteLine($"Course: {student1.Course}");
+                Console.WriteLine($"GPA: {student1.GPA}");
+
+                Console.WriteLine("\nStudent 2 Details:");
+                Console.WriteLine($"Name: {student2.Name}");
+                Console.WriteLine($"Age: {student2.Age}");
+                Console.WriteLine($"Course: {student2.Course}");
+                Console.WriteLine($"GPA: {student2.GPA}");
+            Console.ReadLine();
+            }
+        }
+    }
+ 
